3bfc78aa22c693b7226f99b59be3ebd6d24c9f6b
Areatomic <aureliensebe@gmail.com>
2020-05-11 11:04:04 +0200

    Body and importation of images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import Debout from './full.png';
+import Marvel from './marvel.jpg';
+import Genou from './genou.png';
+import Kick from './kick.png';
+import Griffe from './griffe.png';
+import ui from './ui.svg';
 import './App.css';
 
 function App() {
@@ -42,8 +48,24 @@ function App() {
           <span></span>
         </div>
       </div>
-      <div className='main'></div>
-      <div className='footer'></div>
+      <div className='main'>
+        <h1>
+          BLACK
+          <br />
+          <span>PANTHER</span>
+        </h1>
+        <img src={Debout} alt='black panther' className='debout' />
+      </div>
+      <div className='footer'>
+        <img src={Marvel} className='marvel-image' />
+        <div className='marvel'></div>
+        <div className='nav'>
+          <button>
+            <img src={ui} />
+            <text>MORE</text>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
